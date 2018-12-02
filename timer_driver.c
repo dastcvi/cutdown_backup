17be738bc209724ed8e0d3c7fb489c74d3f321a4
dastcvi <david.stclair@colorado.edu>
2018-12-02 11:09:47 -0700

    Added timer functions

--- timer_driver.c
+++ timer_driver.c
@@ -11,21 +11,84 @@
 
 /* note: the system clock is shipped with a default of the 8 MHz internal RC prescaled down to 1 MHz */
 
-void timer_init(void)
+volatile uint16_t timer_value = 0;
+volatile bool timer_enable = false;
+
+void init_timer(void)
 {
-	// 1 Mhz clock / 8 (prescale) / 125 (compare) = 1 ms interrupt
+	// 1 Mhz clock / 8192 (prescale) / 122 (compare) = 0.9994 ms interrupt
 	/* set to clear timer on match with OCR1C register */
 	_SFR_BYTE(TCCR1) |= 1 << CTC1;
 	
-	/* load OCR1A and OCR1C with 124 for 1 ms interrupt */
-	_SFR_BYTE(OCR1A) = 124; /* for interrupt */
-	_SFR_BYTE(OCR1C) = 124; /* for timer clear */
+	/* load OCR1A and OCR1C with 121 for 1 s interrupt */
+	_SFR_BYTE(OCR1A) = 121; /* for interrupt */
+	_SFR_BYTE(OCR1C) = 121; /* for timer clear */
 	
 	/* enable interrupt on OCR1A match */
 	_SFR_BYTE(TIMSK) |= 1 << OCIE1A;
 	
 	/* start clock */
-	_SFR_BYTE(TCCR1) |= 1 << CS12; /* select CS1[3:0] = 1b0010 => CK/8 prescale */
+	_SFR_BYTE(TCCR1) |= 0b1110; /* select CS1[3:0] = 0b1110 => CK/8192 prescale */
 }
 
-// todo: interrupt handler
+void set_timer_hi(uint8_t value)
+{
+	cli();
+	timer_value &= (uint16_t) 0x00ff;
+	timer_value |= ((uint16_t) value) << 8;
+	sei();
+}
+
+void set_timer_lo(uint8_t value)
+{
+	cli();
+	timer_value &= (uint16_t) 0xff00;
+	timer_value |= (uint16_t) value;
+	sei();
+}
+
+uint16_t read_timer(void)
+{
+	uint16_t value = 0;
+	
+	cli();
+	value = timer_value;
+	sei();
+	
+	return value;
+}
+
+void enable_timer()
+{
+	cli();
+	timer_enable = true;
+	sei();
+}
+
+void disable_timer()
+{
+	cli();
+	timer_enable = false;
+	sei();
+}
+
+ISR (TIMER1_COMPA_vect)
+{
+	static bool gpio = false;
+	
+	cli();
+	if (timer_enable) {
+		if (timer_value > 0) {
+			timer_value--;
+		}
+	}
+	sei();
+	
+	if (gpio) {
+		_SFR_BYTE(PORTB) &= ~(1 << PB4); /* Write PB4 low */
+		gpio = false;
+	} else {
+		_SFR_BYTE(PORTB) |= 1 << PB4;
+		gpio = true;
+	}
+}
--- timer_driver.h
+++ timer_driver.h
@@ -10,8 +10,15 @@
 #define TIMER_DRIVER_H_
 
 #include <stdint.h>
+#include <stdbool.h>
 
-void timer_init(void);
+void init_timer(void);
 
+void set_timer_hi(uint8_t value);
+void set_timer_lo(uint8_t value);
+uint16_t read_timer(void);
+
+void enable_timer();
+void disable_timer();
 
 #endif /* TIMER_DRIVER_H_ */
