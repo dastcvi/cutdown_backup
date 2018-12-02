16fb80874aadb40377cd0f5b4583bda4f0ef7563
dastcvi <david.stclair@colorado.edu>
2018-12-02 13:12:35 -0700

    Added check if the primary MCU has fired a squib

--- main.c
+++ main.c
@@ -15,8 +15,10 @@ volatile uint16_t iter = 0;
 
 int main(void)
 {
-	_SFR_BYTE(DDRB) |= 1 << DDB4; /* Configure PB4 as output */
-	_SFR_BYTE(PORTB) &= ~(1 << PB4); /* Write PB4 low */
+	_SFR_BYTE(DDRB) |= 1 << DDB4; /* Configure PB4 (squib gate) as output */
+	_SFR_BYTE(PORTB) &= ~(1 << PB4); /* Write squib gate low */
+	
+	_SFR_BYTE(PORTB) |= (1 << PB3); /* Set PB3 (primary squib fired) as input */
 	
 	init_spi_slave();
 	init_timer();
@@ -26,11 +28,14 @@ int main(void)
 	
     while (!timer_expired);
 	
-	_SFR_BYTE(PORTB) |= (1 << PB4); /* Write PB4 high */
+	/* check if the primary MCU has fired a squib before firing the backup */
+	if (((_SFR_BYTE(PINB) >> PB3) & 1) == 0) {
+		_SFR_BYTE(PORTB) |= (1 << PB4); /* Write squib gate high */
 	
-	for (iter = 0; iter < 40000; iter++);
+		for (iter = 0; iter < 40000; iter++);
 	
-	_SFR_BYTE(PORTB) &= ~(1 << PB4); /* Write PB4 low */
+		_SFR_BYTE(PORTB) &= ~(1 << PB4); /* Write squib gate low */
+	}
 	
 	while(1);
 }
